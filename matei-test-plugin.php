2a6b96c3f6d1305066578fc650715eaddbb4921b
Matei7 <vladmatei96@yahoo.com>
2018-08-29 11:20:34 +0300

    admin-menu

--- matei-test-plugin.php
+++ matei-test-plugin.php
@@ -1,12 +1,43 @@
 <?php
+/*
+Plugin Name: Wordpress Test Plugin
+Plugin URI: https://wordpress.org/plugins/health-check/
+Description: Checks the health of your WordPress install
+Version: 0.1.0
+Author: The Health Check Team
+Author URI: http://health-check-team.example.com
+Text Domain: health-check
+Domain Path: /languages
+*/
+
 
 function load_css()
 {
-    wp_register_style('style', plugin_dir_url("wordpress-test-plugin/css/style.css"));
-    wp_enqueue_style('style');
+    wp_enqueue_style('wp-test-plugin-css', plugin_dir_url(__FILE__) . "css/matei-test-plugin.css");
+}
+
+
+function test_plugin_callback()
+{
+    echo '<h1>Test plugin </h1 >';
+}
+
+
+function test_plugin_widget()
+{
+    add_menu_page(
+        __('Custom Menu Title', 'textdomain'),
+        'Test plugin',
+        'manage_options',
+        plugin_dir_url(__FILE__) . "matei-test-plugin.php",
+        'test_plugin_callback',
+        100
+    );
 }
 
 
 if (!is_admin()) {
     add_action('wp_enqueue_scripts', 'load_css');
+} else {
+    add_action('admin_menu', 'test_plugin_widget');
 }
