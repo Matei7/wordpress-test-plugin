c8640bafed0f5dd3b6d5fab56e1947a90d4e9dd2
Matei7 <vladmatei96@yahoo.com>
2018-08-31 14:38:33 +0300

    slider

--- js/myscript.js
+++ js/myscript.js
@@ -7,12 +7,6 @@ jQuery(document).ready(function ($) {
     $height = $("#line-height");
     $spacing = $("#letter-spacing");
 
-    $height.change(function () {
-        $heightSlider.slider("value", $(this).val());
-    });
-    $spacing.change(function () {
-        $spacingSlider.slider("value", $(this).val());
-    });
 
     $heightSlider.slider({
         range: "max",
@@ -33,8 +27,33 @@ jQuery(document).ready(function ($) {
         }
     });
 
-    $('#min-height').text($heightSlider.slider("option", "min"));
-    $('#min-spacing').text($spacingSlider.slider("option", "min"));
-    $('#max-height').text($heightSlider.slider("option", "max"));
-    $('#max-spacing').text($spacingSlider.slider("option", "max"));
+    $minHeight = $heightSlider.slider("option", "min");
+    $maxHeight = $heightSlider.slider("option", "max");
+    $minSpacing = $spacingSlider.slider("option", "min");
+    $maxSpacing = $spacingSlider.slider("option", "max");
+
+    $('#min-height').text($minHeight);
+    $('#min-spacing').text($minSpacing);
+    $('#max-height').text($maxHeight);
+    $('#max-spacing').text($maxSpacing);
+
+
+    $height.change(function () {
+        $value = $(this).val();
+        $heightSlider.slider("value", $value);
+        if ($value > $maxHeight) {
+            $height.val($maxHeight);
+        } else if ($value < $minHeight) {
+            $height.val($minHeight);
+        }
+    });
+    $spacing.change(function () {
+        $value = $(this).val();
+        $spacingSlider.slider("value", $value);
+        if ($value > $maxSpacing) {
+            $spacing.val($maxSpacing);
+        } else if ($value < $minSpacing) {
+            $spacing.val($minSpacing);
+        }
+    });
 });
